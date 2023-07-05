936779ffb11bb4033ccabdaec3cc4a3d9d4c8c41
JabriMuhi <73785841+JabriMuhi@users.noreply.github.com>
2023-07-05 16:02:40 +0300

    feat: added logger and new routes

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,36 +2,51 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 	"strconv"
 )
 
-// Обработчик главной страницы.
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w) // Использование помощника notFound()
 		return
 	}
-	w.Write([]byte("Привет!"))
+
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err) // Использование помощника serverError()
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		app.serverError(w, err) // Использование помощника serverError()
+	}
 }
 
-// Обработчик для отображения содержимого заметки.
-func showSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w) // Использование помощника notFound()
 		return
 	}
-	fmt.Fprintf(w, "Отображение выбранной заметки с ID %d...", id)
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-// Обработчик для создания новой заметки.
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-
-		http.Error(w, "Метод запрещен!", 405)
+		app.clientError(w, http.StatusMethodNotAllowed) // Используем помощник clientError()
 		return
 	}
-	w.Write([]byte("Форма для создания новой заметки..."))
+
+	w.Write([]byte("Создание новой заметки..."))
 }
