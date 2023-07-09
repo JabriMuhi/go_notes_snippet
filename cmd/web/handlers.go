f33d637a5699699eca8ef3f488f7bf5551cc5ce9
JabriMuhi <73785841+JabriMuhi@users.noreply.github.com>
2023-07-09 18:51:35 +0300

    feat: added methods for notes

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,34 +1,45 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"html/template"
+	"go_notes_snippet/pkg/models"
 	"net/http"
 	"strconv"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		app.notFound(w) // Использование помощника notFound()
+		app.notFound(w)
 		return
 	}
 
-	files := []string{
-		"./ui/html/home.page.tmpl",
-		"./ui/html/base.layout.tmpl",
-		"./ui/html/footer.partial.tmpl",
-	}
-
-	ts, err := template.ParseFiles(files...)
+	s, err := app.notes.Latest()
 	if err != nil {
-		app.serverError(w, err) // Использование помощника serverError()
+		app.serverError(w, err)
 		return
 	}
 
-	err = ts.Execute(w, nil)
-	if err != nil {
-		app.serverError(w, err) // Использование помощника serverError()
+	for _, snippet := range s {
+		fmt.Fprintf(w, "%v\n", snippet)
 	}
+
+	// files := []string{
+	//     "./ui/html/home.page.tmpl",
+	//     "./ui/html/base.layout.tmpl",
+	//     "./ui/html/footer.partial.tmpl",
+	// }
+
+	// ts, err := template.ParseFiles(files...)
+	// if err != nil {
+	//     app.serverError(w, err)
+	//     return
+	// }
+
+	// err = ts.Execute(w, nil)
+	// if err != nil {
+	//     app.serverError(w, err)
+	// }
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
@@ -38,7 +49,18 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	s, err := app.notes.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	// Отображаем весь вывод на странице.
+	fmt.Fprintf(w, "%v", s)
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
@@ -48,5 +70,17 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("Создание новой заметки..."))
+	title := "История про улитку"
+	content := "Улитка выползла из раковины,\nвытянула рожки,\nи опять подобрала их."
+	expires := "7"
+
+	// Передаем данные в метод SnippetModel.Insert(), получая обратно
+	// ID только что созданной записи в базу данных.
+	id, err := app.notes.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
 }
--- pkg/models/mysql/notes.go
+++ pkg/models/mysql/notes.go
@@ -2,6 +2,7 @@ package mysql
 
 import (
 	"database/sql"
+	"errors"
 	"go_notes_snippet/pkg/models"
 )
 
@@ -12,15 +13,105 @@ type NoteModel struct {
 
 // Insert - Метод для создания новой заметки в базе дынных.
 func (m *NoteModel) Insert(title, content, expires string) (int, error) {
-	return 0, nil
+	stmt := `INSERT INTO snippets (title, content, created, expires)
+    VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
 }
 
 // Get - Метод для возвращения данных заметки по её идентификатору ID.
 func (m *NoteModel) Get(id int) (*models.Note, error) {
-	return nil, nil
+	// SQL запрос для получения данных одной записи.
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+    WHERE expires > UTC_TIMESTAMP() AND id = ?`
+
+	// Используем метод QueryRow() для выполнения SQL запроса,
+	// передавая ненадежную переменную id в качестве значения для плейсхолдера
+	// Возвращается указатель на объект sql.Row, который содержит данные записи.
+	row := m.DB.QueryRow(stmt, id)
+
+	// Инициализируем указатель на новую структуру Snippet.
+	s := &models.Note{}
+
+	// Используйте row.Scan(), чтобы скопировать значения из каждого поля от sql.Row в
+	// соответствующее поле в структуре Snippet. Обратите внимание, что аргументы
+	// для row.Scan - это указатели на место, куда требуется скопировать данные
+	// и количество аргументов должно быть точно таким же, как количество
+	// столбцов в таблице базы данных.
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	if err != nil {
+		// Специально для этого случая, мы проверим при помощи функции errors.Is()
+		// если запрос был выполнен с ошибкой. Если ошибка обнаружена, то
+		// возвращаем нашу ошибку из модели models.ErrNoRecord.
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, models.ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+
+	// Если все хорошо, возвращается объект Snippet.
+	return s, nil
 }
 
 // Latest - Метод возвращает 10 наиболее часто используемые заметки.
 func (m *NoteModel) Latest() ([]*models.Note, error) {
-	return nil, nil
+	// Пишем SQL запрос, который мы хотим выполнить.
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+    WHERE expires > UTC_TIMESTAMP() ORDER BY created DESC LIMIT 10`
+
+	// Используем метод Query() для выполнения нашего SQL запроса.
+	// В ответ мы получим sql.Rows, который содержит результат нашего запроса.
+	rows, err := m.DB.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+
+	// Откладываем вызов rows.Close(), чтобы быть уверенным, что набор результатов из sql.Rows
+	// правильно закроется перед вызовом метода Latest(). Этот оператор откладывания
+	// должен выполнится *после* проверки на наличие ошибки в методе Query().
+	// В противном случае, если Query() вернет ошибку, это приведет к панике
+	// так как он попытается закрыть набор результатов у которого значение: nil.
+	defer rows.Close()
+
+	// Инициализируем пустой срез для хранения объектов models.Snippets.
+	var snippets []*models.Note
+
+	// Используем rows.Next() для перебора результата. Этот метод предоставляем
+	// первый а затем каждую следующею запись из базы данных для обработки
+	// методом rows.Scan().
+	for rows.Next() {
+		// Создаем указатель на новую структуру Snippet
+		s := &models.Note{}
+		// Используем rows.Scan(), чтобы скопировать значения полей в структуру.
+		// Опять же, аргументы предоставленные в row.Scan()
+		// должны быть указателями на место, куда требуется скопировать данные и
+		// количество аргументов должно быть точно таким же, как количество
+		// столбцов из таблицы базы данных, возвращаемых вашим SQL запросом.
+		err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+		if err != nil {
+			return nil, err
+		}
+		// Добавляем структуру в срез.
+		snippets = append(snippets, s)
+	}
+
+	// Когда цикл rows.Next() завершается, вызываем метод rows.Err(), чтобы узнать
+	// если в ходе работы у нас не возникла какая либо ошибка.
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	// Если все в порядке, возвращаем срез с данными.
+	return snippets, nil
 }
